3614f48c4bcfbfdb52907009051ac9a49ff48912
Arthur Lage <arthurlage2006@gmail.com>
2026-04-01 17:27:28 -0300

    chore(web): adicionar primevue no projeto

--- apps/web/src/App.vue
+++ apps/web/src/App.vue
@@ -1,10 +1,13 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { Button } from 'primevue';
+</script>
 
 <template>
   <h1 class="text-xl font-bold">Vincis</h1>
   <p class="text-red-500">
     Um app pra lacrar nos concursos
   </p>
+  <Button>Botão prime vue</Button>
 </template>
 
 <style scoped></style>
--- apps/web/src/main.ts
+++ apps/web/src/main.ts
@@ -1,12 +1,20 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { PrimeVue } from '@primevue/core'
+import Aura from '@primeuix/themes/aura'
 
 import App from './App.vue'
 import router from './router'
+import { Button } from 'primevue'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(PrimeVue, {
+    theme: {
+        preset: Aura
+    }
+})
 
 app.mount('#app')
