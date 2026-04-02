bdd1a00df2c8461cf2d28798316d558ead387858
Igor Peixoto <igorpeixoto_@outlook.com.brm>
2026-04-02 15:58:30 -0300

    feat(api): add user feature with controller, model, service, routes, and types
    - Added user.controller.ts for handling user-related requests.
    - Created user.model.ts to define the user data structure.
    - Implemented user.routes.ts for user endpoint definitions.
    - Developed user.service.ts for business logic related to users.
    - Introduced user.types.ts for TypeScript type definitions related to users.
    - Updated package.json to include a new script for backend development.

--- apps/api/index.js
+++ apps/api/index.js
@@ -7,4 +7,8 @@ app.get("/", (req, res) => {
     return res.send("oie")
 })
 
+app.get("/status", (req, res) => {
+    return res.send("tamo on e roteando")
+})
+
 app.listen(PORT, () => { console.log("Listening on port: " + PORT)})
